8aa9e5d0dc392063ce7e30a0b8c6851fd05cb8ed
Alexander <loco.cansado@gmail.com>
2021-10-31 12:32:56 -0300

    Debido a que tuve problemas al guardar los archivos con su nombre original (quedaban con 0kb) les cambié el nombre a su HASH, pero en caso de querer recuperar este nombre, para cada carpeta hay un MAPEO desde el archivo actual al archivo original.
    -----------------------
    Agregadas funciones para
    "descargar archivos"
    "eliminar archivos"

--- EnlaceFTP.py
+++ EnlaceFTP.py
@@ -1,8 +1,19 @@
 import ftplib
+import hashlib
+from os import remove
+import csv
+from datetime import datetime
 
 #conectará con el FTP
 #moverá entre dominios
 #moverá entre usuarios
+
+def agregar_mapa_archivo(origen, destino, archivo = "filemap.csv"):
+    with open(archivo, "a", newline='') as f:
+        w = csv.DictWriter(f, fieldnames = ["origen", "destino"])
+        w.writerow({"origen" : origen, "destino" : destino})
+
+
 class EnlaceFTP:
 
     def __init__(self, parametros):
@@ -20,6 +31,14 @@ class EnlaceFTP:
         ftp = ftplib.FTP(self.__host)
         ftp.login(self.__usuario, self.__pass)
         return ftp
+
+    @property
+    def archivos_transferidos(self):
+        return self.__archivos_transferidos
+
+    @property
+    def archivos_eliminados(self):
+        return self.__archivos_eliminados
 
     @property
     def dominios(self):
@@ -51,19 +70,73 @@ class EnlaceFTP:
         conexion.quit()
         return ret
 
-    def listas_archivos_usuario(self, usuario, dominio = None, directorios = ["cur", "new"]):
+    def listas_archivos_usuario(self, usuario, dominio = None, directorio = "cur"):
         ret = []
         conexion = self.__conexion()
         if dominio is None:
             dominio = self.__dominio
-        for directorio in directorios:
-            conexion.cwd(f"/{dominio}/{usuario}/{directorio}")
-            data = []
-            conexion.retrlines('MLSD', data.append)
-            for line in data:
-                filename = line.split(";")[-1].strip()
-                tipo = line.split(";")[0].split("=")[1].strip()
-                if((filename != '.' or filename != '..') and tipo == "file"):
-                    ret.append(filename)
+        conexion.cwd(f"/{dominio}/{usuario}/{directorio}")
+        data = []
+        conexion.retrlines('MLSD', data.append)
+        for line in data:
+            filename = line.split(";")[-1].strip()
+            tipo = line.split(";")[0].split("=")[1].strip()
+            if((filename != '.' or filename != '..') and tipo == "file"):
+                ret.append(filename)
         conexion.quit()
         return ret
+
+#eliminará todos los archivos que se encuentre a N dias del día de hoy
+    def elimina_archivo_usuario_meses(self, usuario, dias = 180,dominio = None, directorio = "cur"):
+        fecha_actual_timestamp = datetime.now(tz=None).timestamp()
+        ret = []
+        conexion = self.__conexion()
+        if dominio is None:
+            dominio = self.__dominio
+        conexion.cwd(f"/{dominio}/{usuario}/{directorio}")
+        data = []
+        conexion.retrlines('MLSD', data.append)
+        for line in data:
+            filename = line.split(";")[-1].strip()
+            tipo = line.split(";")[0].split("=")[1].strip()
+            if((filename != '.' or filename != '..') and tipo == "file"):
+                fecha_archivo_timestamp = int(filename.split(".")[0])
+                if fecha_archivo_timestamp < (fecha_actual_timestamp - dias*24*60*60):
+                    conexion.delete(filename)
+                    self.__archivos_eliminados += 1
+                
+        conexion.quit()
+
+    def descargar_archivos(self, usuario, dominio, directorio, lista_archivos):
+        conexion = self.__conexion()
+        conexion.cwd(f"/{dominio}/{usuario}/{directorio}")
+        for archivo in lista_archivos:
+            self.descargar_archivo(archivo, conexion)
+        conexion.quit()
+
+    def descargar_archivo(self, nombre_archivo, enlace):
+        err = False
+        file_size = enlace.size(nombre_archivo)
+        nombre_string = f"{nombre_archivo.split('.')[0]}.{nombre_archivo.split('.')[1]}.{hashlib.md5(nombre_archivo.encode()).hexdigest()}.eml"
+        file = open(nombre_string, 'wb')
+        while file_size != file.tell():
+            try:
+                if file.tell() != 0:
+                    enlace.retrbinary(f'RETR {nombre_archivo}', file.write, file.tell())
+                else:
+                    enlace.retrbinary(f'RETR {nombre_archivo}', file.write)
+            except Exception as e:
+                print(f"error en {nombre_archivo} : {e}")
+                file.close()
+                err = True
+                remove(nombre_string)
+                break
+        if(not err):
+            file.close()
+            agregar_mapa_archivo(nombre_string, nombre_archivo)
+            self.__archivos_transferidos += 1
+        
+
+
+
+
